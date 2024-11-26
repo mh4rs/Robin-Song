e8e032b84551c9ffd405ede1d63cb1c41d42eb63
jodi <128635337+jodi-j@users.noreply.github.com>
2024-11-25 22:39:57 -0500

    Jodi Joven - Frontend Cleanup & Package Updates (#1)

--- App.js
+++ App.js
@@ -1,4 +1,24 @@
-import { registerRootComponent } from 'expo';
-import TabNavigator from './frontend/app/TabNavigator';
+//import { registerRootComponent } from 'expo';
+//import TabNavigator from './frontend/app/TabNavigator';
 
-registerRootComponent(TabNavigator);
+//registerRootComponent(TabNavigator);
+
+import React from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
+export default function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Robin</Text>
+       </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F3E1D6',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
