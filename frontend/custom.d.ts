6ba591fd5369fb6cb3f1fb28da71d5c900483293
jodi <128635337+jodi-j@users.noreply.github.com>
2024-12-01 23:10:07 -0500

    Jodi Joven - Frontend ChatGPT Modal Basic Setup (#3)

--- frontend/custom.d.ts
+++ frontend/custom.d.ts
@@ -1,3 +1,4 @@
 declare module '@react-navigation/native';
 declare module '@react-navigation/bottom-tabs';
 declare module 'react-native-vector-icons/Ionicons';
+declare module 'react-native-vector-icons/MaterialCommunityIcons';
